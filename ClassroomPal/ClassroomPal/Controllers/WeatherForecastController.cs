f69555dc19c4007a8eb19c5c638f9bde21cebd0c
Ravi Singh <rsingh22@Knights.ucf.edu>
2022-05-13 00:06:14 -0400

    Pushing .NET 6 project with class library example

---
+++ ClassroomPal/ClassroomPal/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using CPCore;
+
+namespace ClassroomPal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public bool IsUpperCase(string value)
+        {
+            return StringLibrary.StartsWithUpper(value);
+        }
+
+        [HttpGet]
+        public string ToLowerCase(string value)
+        {
+            return StringLibrary.ConvertToLower(value);
+        }
+    }
+}
---
+++ Kernel/CPCore/CPCore/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace CPCore
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+
+        public static string ConvertToLower(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return "";
+
+            return str.ToLower();
+        }
+    }
+}
